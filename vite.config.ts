11adbf456d0293a97d083ed15abf10b46dd5a3eb
benoitrastier <benoit.r@obat.fr>
2025-01-21 15:48:33 +0100

    - Add types

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  lib: {
+    entry: resolve(__dirname, 'src/index.ts'),
+    name: 'tanstack-query-entity-graph',
+    fileName: 'index'
+  },
   plugins: [react()],
 })
