ea1b414e66082c1832f39f01db7c9130b9fed325
benoitrastier <benoit.r@obat.fr>
2025-01-28 17:50:51 +0100

    - Add vite-dts plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
+import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
   lib: {
-    entry: resolve(__dirname, 'src/index.ts'),
+    entry: resolve(__dirname, 'src/lib/index.ts'),
+    formats: ['es', 'umd'],
     name: 'tanstack-query-entity-graph',
-    fileName: 'index'
+    fileName: (format) =>
+        format === 'umd'
+            ? 'tanstack-query-entity-graph.umd.cjs'
+            : 'tanstack-query-entity-graph.js'
   },
-  plugins: [react()],
+  plugins: [react(), dts({ include: ["./src/lib"]})],
 })
